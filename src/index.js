58c8a0b1dd734d18ab9283a8dd0e269b86c6cdfa
ninojovic <ninojovic@gmail.com>
2019-10-26 10:42:35 +0200

    UI Setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import { fabric } from 'fabric';
+import params from './params';
 
-console.log(fabric.version);
+const { BOARD_WIDTH, BOARD_HEIGHT, BOX_SIZE } = params;
+
+const drawCanvas = canvasDOMSelector => new fabric.Canvas(canvasDOMSelector, {
+	height:	BOARD_HEIGHT,
+	width: BOARD_WIDTH,
+	selection: false,
+});
+
+const drawGridLines = (canvas) => {
+	const lineOptions = {
+		stroke: '#EEEEEE',
+		strokeWidth: 1,
+		selectable: false,
+		hoverCursor: 'default',
+	};
+
+	for (let i = 1; i < (canvas.width / BOX_SIZE); i++) {
+		canvas.add(new fabric.Line([BOX_SIZE * i, 0, BOX_SIZE * i, canvas.width], lineOptions));
+		canvas.add(new fabric.Line([0, BOX_SIZE * i, canvas.height, BOX_SIZE * i], lineOptions));
+	}
+};
+
+const canvas = drawCanvas('game-canvas');
+drawGridLines(canvas);
---
+++ src/params.js
@@ -0,0 +1,5 @@
+export default {
+	BOARD_WIDTH: 600,
+	BOARD_HEIGHT: 600,
+	BOX_SIZE: 60,
+};
